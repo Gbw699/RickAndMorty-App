fcce1a0909bb9b9779ea9687de9430895fc88647
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-11-28 21:23:18 -0300

    fixed divs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import './App.css'
-import Card from './components/Card.jsx'
-import Cards from './components/Cards.jsx'
-import SearchBar from './components/SearchBar.jsx'
-import characters, { Rick } from './data.js'
+import "./App.css";
+import Card from "./components/Card.jsx";
+import Cards from "./components/Cards.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import characters, { Rick } from "./data.js";
 
-function App () {
+function App() {
   return (
-    <div className='App' style={{ padding: '25px' }}>
+    <div className="App" style={{ padding: "25px" }}>
       <div>
         <Card
           name={Rick.name}
           species={Rick.species}
           gender={Rick.gender}
           image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
         />
       </div>
       <hr />
-      <div>
-        <Cards
-          characters={characters}
-        />
-      </div>
+      <Cards characters={characters} />
       <hr />
       <div>
-        <SearchBar
-          onSearch={(characterID) => window.alert(characterID)}
-        />
+        <SearchBar onSearch={(characterID) => window.alert(characterID)} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,15 +3,14 @@ import Card from "./Card";
 export default function Cards(props) {
   const { characters } = props;
   const element = characters.map((character, index) => (
-    <div key={index}>
-      <Card
-        name={character.name}
-        species={character.species}
-        gender={character.gender}
-        image={character.image}
-        onClose={() => window.alert("Emulamos que se cierra la card")}
-      />
-    </div>
+    <Card
+      name={character.name}
+      species={character.species}
+      gender={character.gender}
+      image={character.image}
+      onClose={() => window.alert("Emulamos que se cierra la card")}
+      key={index}
+    />
   ));
   return <div>{element}</div>;
 }
