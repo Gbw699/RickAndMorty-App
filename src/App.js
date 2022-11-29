3ffa58e955a7cbd36b0bd9a6253db8b288fe797f
Gabriel Barimboim <gdbarimboim@gmail.com>
2022-11-29 10:05:48 -0300

    fixed with code review 06 02

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import "./App.css";
-import Card from "./components/Card.jsx";
-import Cards from "./components/Cards.jsx";
-import SearchBar from "./components/SearchBar.jsx";
-import characters, { Rick } from "./data.js";
+import Cards from "./components/Cards";
+import SearchBar from "./components/SearchBar";
+import characters from "./data";
 
 function App() {
   return (
     <div className="App" style={{ padding: "25px" }}>
-      <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert("Emulamos que se cierra la card")}
-        />
-      </div>
       <hr />
       <Cards characters={characters} />
       <hr />
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,10 @@
 export default function SearchBar(props) {
   return (
     <div>
-      <input type="search" />
-      <button onClick={props.onSearch("Texto para remplazar con characterID")}>
+      <input type="search" name="search" id="" />
+      <button
+        onClick={() => props.onSearch("Texto para remplazar con characterID")}
+      >
         Agregar
       </button>
     </div>
